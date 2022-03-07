637839cde18194562cf3ef3a134de6ed25968503
José Trejo <joseatrejos>
2022-03-07 15:21:59 -0700

    Initial Commit

---
+++ EncuestaCovid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EncuestaCovid
+//
+//  Created by Alumno on 3/7/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
