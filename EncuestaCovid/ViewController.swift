bbbf1039048046a584b1c276aeb53a5e46d0adbe
José Trejo <joseatrejos>
2022-03-14 17:46:57 -0700

    Interface navegable

--- EncuestaCovid/ViewController.swift
+++ EncuestaCovid/ViewController.swift
@@ -14,23 +14,65 @@ class ViewController: UIViewController {
     @IBOutlet weak var profesorButton: UIButton!
     @IBOutlet weak var empleadoButton: UIButton!
     @IBOutlet weak var matriculalbl: UILabel!
+    @IBOutlet weak var professorButtonA: UIButton!
+    
+    @IBOutlet weak var QuestionOneS: UISegmentedControl!
+    @IBOutlet weak var QuestionTwoS: UISegmentedControl!
+    @IBOutlet weak var QuestionThreeS: UISegmentedControl!
+    @IBOutlet weak var QuestionFourS: UISegmentedControl!
+    @IBOutlet weak var QuestionFiveS: UISegmentedControl!
+    @IBOutlet weak var QuestionSixS: UISegmentedControl!
+    @IBOutlet weak var QuestionSevenS: UISegmentedControl!
+    @IBOutlet weak var QuestionEightS: UISegmentedControl!
+    @IBOutlet weak var QuestionNineS: UISegmentedControl!
+    
+    @IBOutlet weak var userIdentificationtxt: UITextField!
+    @IBOutlet weak var userNametxt: UITextField!
+    @IBOutlet weak var userEmailtxt: UITextField!
+    
+    @IBOutlet weak var warninglbl: UILabel!
     
     
-    @IBAction func alumnoIsPressed (Sender: UIButton) {
+    @IBAction func btnEnviar(_ sender: Any) {
+        
+        
+        if  userIdentificationtxt.text != nil || userNametxt.text != nil ||  userEmailtxt.text != nil {
+        
+        warninglbl.text = "Rellena todos los campos para continuar"
+        
+        } else {
+            
+            let message:String = "Encuesta enviada correctamente. Recibiras el codigo QR de acceso en tu email"
+            
+            let alertController:UIAlertController = UIAlertController(title: "Encuesta Covid Enviada", message: message, preferredStyle: UIAlertController.Style.alert)
+            
+            let alertAction:UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:nil)
+            
+            // Add alertAction object to alertController.
+            alertController.addAction(alertAction)
+            // Popup the alert dialog.
+            present(alertController, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    @IBAction func alumnoButtonA(_ sender: Any) {
         matriculalbl.text = "Matricula"
+    }
+    @IBAction func profesorButtonA(_ sender: Any) {
+        matriculalbl.text = "Numero de profesor"
         
     }
     
-    @IBAction func profesorIsPressed (Sender: UIButton) {
-          matriculalbl.text = "Numero de profesor"
-          
-      }
-    
-    @IBAction func empleadoIsPressed (Sender: UIButton) {
+    @IBAction func empleadoButtonA(_ sender: Any) {
         matriculalbl.text = "Numero de empleado"
-        
     }
-      
+    
+    
+    
+    
+    
     
     
     
@@ -43,6 +85,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
